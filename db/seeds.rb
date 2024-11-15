68bc887965ec91afda4e0a276b71726d4737c854
loris morlais <loris.morlais@icloud.com>
2024-11-15 12:52:29 +0900

    setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.destroy_all
+
+require 'net/http'
+require 'json'
+
+url = URI("https://tmdb.lewagon.com/movie/top_rated")
+response = Net::HTTP.get(url)
+movies = JSON.parse(response)["results"].sample(15)  # Get 15 random movies
+
+movies.each do |movie_data|
+  Movie.find_or_create_by!(title: movie_data["title"]) do |movie|
+    movie.overview = movie_data["overview"] if movie_data["overview"].present?
+    movie.poster_url = movie_data["poster_path"] if movie_data["poster_path"].present?
+    movie.rating = movie_data["vote_average"] if movie_data["vote_average"].present?
+  end
+end
+
+puts "Created #{Movie.count} movies"
